a48ffb1d649a55572f21ca94f53192b7bd3a3fa2
Leo <leo.A.mottay@gmail.com>
2017-06-21 22:10:50 +0200

    modification sur le controlleur des commandes

--- src/application/controller/commandeController.java
+++ src/application/controller/commandeController.java
@@ -6,19 +6,20 @@ import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.ListView;
-import javafx.scene.control.RadioButton;
+import javafx.scene.control.*;
 
 public class commandeController implements Initializable {
 
-	@FXML ListView idListAttribuerTracteur;
-	@FXML ListView idListAttribuerMoissoneuse;
-	@FXML RadioButton idRadioButtonCamionCiterneOui;
-	@FXML RadioButton  idRadioButtonCamionCiterneNon;
-	@FXML RadioButton  idRadioButtonVehiculeHescorteOui;
-	@FXML RadioButton  idRadioButtonVehiculeHescorteNon;
-	@FXML Button idBouttonValiderChoix;
+	@FXML TableView viewTracteur;
+	@FXML TableView viewMoissonneuse;
+	@FXML ComboBox champSelector;
+	@FXML ComboBox moissonneuseSelector;
+	@FXML ComboBox tracteurSelector;
+	@FXML RadioButton ouiR;
+	@FXML RadioButton  nonR;
+	@FXML RadioButton  ouiE;
+	@FXML RadioButton  nonE;
+	@FXML Button valider;
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
