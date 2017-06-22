c6d549a3db732b6b18b6fbca3cc1e890de29e3ec
Minhokn <killian.leti@gmail.com>
2017-06-22 22:43:02 +0200

    test

--- src/application/model/Maps.java
+++ src/application/model/Maps.java
@@ -11,23 +11,43 @@ public class Maps {
 	private WebView webView;
     private JSObject javascriptOBJ;
 
+    /**
+     * Genere la map
+     * @param mapHTML
+     */
     public Maps(String mapHTML) {
         chargement(mapHTML);
 
     }
+
+    /**
+     * Ajouter des champs a la map
+     * @param champ
+     */
     public void addChamp(Champ champ){
         javascriptOBJ.call("addChamp",champ.getId_ch(),champ.getAdr_ch(),champ.getCult_ch(),champ.getSurf_ch(),champ.getGPS_ch());
     };
+
+    /**
+     * Mettre la map dans la webview
+     * @param googleMaps
+     */
 	public void setParent(StackPane googleMaps) {
 		googleMaps.getChildren().setAll(webView);
 	}
 
-
+    /**
+     * Enlever la map
+     * @param googleMaps
+     */
     public void enleverParent(StackPane googleMaps) {
         googleMaps.getChildren().remove(webView);
     }
 
-
+    /**
+     * Charger la map
+     * @param url
+     */
 	public void chargement(String url){
         webView = new WebView();
         WebEngine webEngine = webView.getEngine();
